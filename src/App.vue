4d0e3797ce281b453f7628db2f98cb7f2a001c4e
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-07-15 12:24:37 +0200

    componenete header completato

--- src/App.vue
+++ src/App.vue
@@ -58,6 +58,6 @@ export default {
 </script>
 
 <style lang="scss">
-@import './style/common'
+
 
 </style>
--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,10 +1,13 @@
 <template>
-    <div class="header-menu">
-        <img src="../assets/img/logo-sidearea-1.png" alt="logo">
-        <ul>
-            <li v-for="link in links" :key="link">
-                {{link.name}}
+    <div class="header-menu-container flex">
+        <img class="header-logo" src="../assets/img/logo-sidearea-1.png" alt="">
+        <ul class="header-menu flex">
+            <li v-for="link,index in links" :key="index" class="menu-item" :class="{'current' : link.currentElement === true}">
+                <a :href="link.url" >
+                    {{link.name}} 
+                </a>
             </li>
+            <img class="hamburger-menu" src="../assets/svg/svg-1.svg" alt="icon-menu">
         </ul>
     </div>
 </template>
@@ -13,11 +16,32 @@
 export default {
     name:'MyHeader',
     props:{
-        links: Array
+        links:Array
     }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../style/common';
+
+.header-menu-container.flex{
+    padding: 30px;
+    justify-content: space-between;
+    align-items: center;
+    height: 70px;
+    .header-logo{
+        width: 100px;
+        cursor: pointer;
+    }
+    .menu-item{
+        margin: 0 15px;
+        font-size: 20px;
+        
+        & a{
+            color: black;
+        }
+    }
+}
+
 
 </style>
