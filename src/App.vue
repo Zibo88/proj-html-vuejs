1af087700540566481111399f0020601c76678ba
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-07-15 11:32:44 +0200

    creazione primo componente

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <MyHeader :links='menuLinks'/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from './components/MyHeader.vue'
 
 export default {
   name: 'App',
+  data(){
+    return{
+
+      menuLinks:[
+
+        {
+          name:'Home',
+          url:'#',
+          currentElement: true,
+        },
+        {
+          name:'Pages',
+          url:'#',
+          currentElement: false,
+        },
+         {
+          name:'Portfolio',
+          url:'#',
+          currentElement: false,
+        },
+         {
+          name:'Blog',
+          url:'#',
+          currentElement: false,
+        },
+         {
+          name:'Shop',
+          url:'#',
+          currentElement: false,
+        },
+         {
+          name:'Element',
+          url:'#',
+          currentElement: false,
+        },
+
+      ]
+
+
+
+    }
+  },
   components: {
-    HelloWorld
+    MyHeader,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './style/common'
+
 </style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="header-menu">
+        <img src="../assets/img/logo-sidearea-1.png" alt="logo">
+        <ul>
+            <li v-for="link in links" :key="link">
+                {{link.name}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'MyHeader',
+    props:{
+        links: Array
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
